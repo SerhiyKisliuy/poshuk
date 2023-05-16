25f6a504e0d7953134a69e5e86e25424eb7cf72b
Kisliuy <kisluy003@gmail.com>
2023-05-16 15:53:22 +0300

    Створити клас TableModel для моделі таблиці. (Патерн MVC). Додати метод hederData для відображення назви колонок. Виконано. Так же додано вивід данних в таблицю.

--- my_modules/tablemodel.py
+++ my_modules/tablemodel.py
@@ -9,27 +9,32 @@ class TableModel(QtCore.QAbstractTableModel):
 
     def __init__(self, head, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.items = []
-        self.hader = []
+        self.items = []  #Данні
+        self.hader = []  #Шапка
 
+    #Отримує данні таблиці
     def setItems(self, items):
         self.beginResetModel()
         self.items = items
         self.endResetModel()
 
+    #Отримує шапку
     def setHader(self, hader):
         self.beginResetModel()
         self.hader = hader
         self.endResetModel()
 
+    #Вказує кількість рядків в таблиці
     def rowCount(self, *args, **kwargs) -> int:
         # return super().rowCount(*args, **kwargs)  #Вертає кількість строк
         return len(self.items)
 
+    #Вказує кількість колонок (стовбців таблиці)
     def columnCount(self, *args, **kwargs) -> int:
         # return super().columnCount(*args, **kwargs)  #Вертає кількість стовбців
         return len(self.hader)
 
+    #Виводить данні в ячейки
     def data(self, index: QModelIndex, role: QtCore.Qt.DisplayRole):
         # return 'a'
         if not index.isValid():
@@ -38,6 +43,7 @@ class TableModel(QtCore.QAbstractTableModel):
             row = self.items[index.row()]
             return row[index.column()]
 
+    #Виводить шапку
     def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: QtCore.Qt.ItemDataRole):
         if role == QtCore.Qt.ItemDataRole.DisplayRole:
             if orientation == QtCore.Qt.Orientation.Horizontal:
